4483b81d0587aa0bc8fcbd153bd14a290723aa97
Brunamon-t <brunamonteiro927@gmail.com>
2026-04-02 20:52:50 +0100

    Update __init__.py

--- agencia_de_viagens/__init__.py
+++ agencia_de_viagens/__init__.py
@@ -1,1 +1,13 @@
+from .agencia_viagens import (
+    adicionar_viagem,
+    destinos_mais_frequentes,
+    calcular_custo_total,
+    filtrar_por_periodo
+)
 
+from .geolocalizacao import (
+    obter_coordenadas,
+    calcular_distancia,
+    distancia_viagem,
+    gerar_mapa_viagens
+)
